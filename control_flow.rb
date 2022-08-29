af05d33cefdc547d12cbb492228ab96df985e2f1
Win Feigle <win.feigle@gmail.com>
2022-08-28 22:01:13 -0600

    commit tree 253a44e975b150f995c16258574abe91b3d4f954

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,41 @@
 def admin_login(username, password)
-  # your code here
+  username.upcase == "ADMIN" && password == "12345" ? "Access granted" : "Access denied"
+
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 
+    response = "brisk"
+  elsif temperature >= 40 && temperature <= 65
+    response = "a little chilly"
+  elsif temperature > 85
+    response = "too dang hot"
+  else 
+    response = "perfect"
+  end
+  "It's #{response} out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else 
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else
+     puts "Invalid operation!"
+  end
 end
 
